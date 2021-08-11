b90196efa4127c175dc01b7914b65f3265835ef0
YI-SPACES\iris.chen <iris.chen@yi-spaces.com>
2021-08-11 09:23:35 +0800

    test timer method and event

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="list-block">
-    <div class="add-task">
-      <div class="outline">
-        <i class="fas fa-plus"></i>
+  <div class="list-block" @click="closeAddInput($event)">
+    <div
+      class="add-task"
+      :class="{ containerFluid: isAdd }"
+      @click="addTask($event)"
+    >
+      <div
+        class="outline"
+        @click="addToTaskList($event)"
+      >
+        <i v-if="isAdd" class="fas fa-check"></i>
+        <i v-else class="fas fa-plus"></i>
       </div>
-      <span>Add Task</span>
+      <input
+        v-if="isAdd"
+        v-model="addTaskItem"
+        @keyup.enter="addToTaskList($event)"
+        type="text"
+        placeholder="ADD TASK..."
+      />
+      <span v-else>Add Task</span>
     </div>
     <!-- <div class="add-task">
       <div class="outline">
@@ -15,11 +30,14 @@
     <div class="todo-task">
       <h1 class="list-title">TO DO LIST</h1>
       <ul>
-        <li><i class="fas fa-play"></i>1.The First task to do today!</li>
+        <li v-for="(task, index) in toDoTasks" :key="index">
+          <i class="fas fa-play"></i>{{ task }}
+        </li>
+        <!-- <li><i class="fas fa-play"></i>1.The First task to do today!</li>
         <li><i class="fas fa-play"></i>2.The Second task to do today!</li>
         <li><i class="fas fa-play"></i>3.The Third task to do today!</li>
         <li><i class="fas fa-play"></i>4.The Fourth task to do today!</li>
-        <li><i class="fas fa-play"></i>5.The Fifth task to do today!</li>
+        <li><i class="fas fa-play"></i>5.The Fifth task to do today!</li> -->
       </ul>
     </div>
     <div class="done-task">
@@ -36,6 +54,42 @@
 <script>
 export default {
   name: "list",
+  data() {
+    return {
+      isAdd: false,
+      addTaskItem: "",
+      toDoTasks: [],
+    };
+  },
+  methods: {
+    addTask(event) {
+      event.stopPropagation();
+      this.isAdd = true;
+      console.log("test");
+    },
+    closeAddInput(event) {
+      // console.log(event.target)
+      if (event.target.tagName !== "INPUT") {
+        this.isAdd = false;
+      }
+    },
+    addToTaskList(event) {
+      // console.log("新增任務");
+      // console.log(this.$store.state.dataKey);
+      if (this.addTaskItem == "") {
+        return;
+      } else {
+        event.stopPropagation();
+        this.toDoTasks.push(this.addTaskItem);
+        this.addTaskItem = "";
+        console.log(this.isAdd);
+        this.isAdd = false;
+      }
+      console.log(this.addTaskItem);
+      console.log(this.toDoTasks);
+      console.log(this.isAdd);
+    },
+  },
 };
 </script>
 
@@ -48,14 +102,18 @@ export default {
   align-items: flex-end;
   padding: 5rem;
   box-sizing: border-box;
+
   .add-task {
     input {
       background-color: #f4d03f;
       outline: none;
       border: none;
       box-sizing: border-box;
-      width: 90%;
-      //   font-size: 1.5rem;
+      width: 85%;
+      font-size: 1.2rem;
+      font-weight: bold;
+      padding-right: 10px;
+      background: border-box;
       &::-webkit-input-placeholder {
         font-weight: 700;
         font-style: italic;
@@ -65,12 +123,11 @@ export default {
     display: flex;
     align-items: center;
     width: 180px;
-    // width: 100%;
     height: 50px;
     border-radius: 30px;
     background-color: #f4d03f;
     color: #0d151d;
-    justify-content: space-evenly;
+    justify-content: space-between;
     .outline {
       width: 30px;
       height: 30px;
@@ -79,17 +136,22 @@ export default {
       display: flex;
       justify-content: center;
       align-items: center;
+      margin: 0 10px;
       .fas {
         font-size: 1rem;
       }
     }
     span {
       padding: 10px;
-      line-height: 50px;
-      text-align: center;
-      font-size: 1.5rem;
+      // line-height: 50px;
+      text-align: left;
+      font-size: 1.3rem;
       font-weight: bold;
+      margin-right: 10px;
     }
+  }
+  .containerFluid {
+    width: 100%;
   }
   .todo-task,
   .done-task {
